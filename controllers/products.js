1b999e48d23e0cc06feaab38211246985b9a2e64
Kiel <ezekieltalampas.c@gmail.com>
2025-04-25 20:03:06 +0800

    controllers set up

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,32 @@
+const Product = require('../models/Product')
 
 
 const getAllProductsStatic = async(req, res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({ msg: 'products testing route' })
+    const search = 'ab'
+    const products = await Product.find({
+        name: { $regex: search, $options: 'i' },
+    });
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async(req, res) => {
-    res.status(200).json({ msg: 'products route' })
+    const {featured, company, name } = req.query;
+
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if(name) {
+        queryObject.name = { $regex: name, $options: 'i' } 
+
+    }
+    console.log(queryObject);
+    const products = await Product.find(queryObject);
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
